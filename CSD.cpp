9092c806e7763aa2267a2f397d4792c96697f3df
Hemayatullah Sangin <hemayat.sangin@gmail.com>
2023-05-24 19:14:31 +0300

    Added menu
    Added menu and the first add_car method.

--- CSD.cpp
+++ CSD.cpp
@@ -2,12 +2,133 @@
 
 using namespace std;
 
+struct date {
+    int day, month, year;
+};
 
+struct car {
+    int ID;
+    char owner_name[20], owner_surname[20], model[20];
+    struct date reg_date, service_date;
+    char phone_no[20];
+};
+
+struct car_directory {
+    car entries[100];
+    int count;
+} directory;
+
+//function for adding car into the directory...
+void add_car() {
+    if (directory.count == 100) {
+        cout << "Directory is full. Cannot add more cars." << endl;
+        return;
+    }
+
+    car newCar;
+
+    cout << "Enter car ID: ";
+    cin >> newCar.ID;
+
+    cout << "Enter owner's name: ";
+    cin >> newCar.owner_name;
+
+    cout << "Enter owner's surname: ";
+    cin >> newCar.owner_surname;
+
+    cout << "Enter car model: ";
+    cin >> newCar.model;
+
+    cout << "Enter registration date (day month year): ";
+    cin >> newCar.reg_date.day >> newCar.reg_date.month >> newCar.reg_date.year;
+
+    cout << "Enter next service date (day month year): ";
+    cin >> newCar.service_date.day >> newCar.service_date.month >> newCar.service_date.year;
+
+    cout << "Enter owner's phone number: ";
+    cin >> newCar.phone_no;
+
+    directory.entries[directory.count] = newCar;
+    directory.count++;
+
+    cout << "Car added successfully." << endl;
+    cout<<"************************************"<<endl;
+    
+}
 
 int main(){
 	
+
 	cout<<"Car Service Directory!"<<endl;
+  cout<<"********************************************"<<endl;
+	cout<<endl;
 	
+	directory.count = 0;
+    int choice;
+
+    do {
+    	
+        cout << "Menu:" << endl;
+          cout<<"********************************************"<<endl;
+        cout << "1- Add a new car to directory" << endl;
+        cout << "2- Delete a car from the directory" << endl;
+        cout << "3- List available cars in the directory" << endl;
+        cout << "4- Search a car:" << endl;
+        cout << "   A - Search according to car ID" << endl;
+        cout << "   B - Search according to owner's name" << endl;
+        cout << "5- Update car information" << endl;
+        cout << "6- Sort according to car ID" << endl;
+        cout << "7- Sort according to owner's name" << endl;
+        cout << "8- Quit" << endl;
+        cout<<"********************************************"<<endl;
+        cout<<endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch (choice) {
+            case 1:
+                add_car();
+                break;
+            case 2:
+//                delete_car();
+                break;
+            case 3:
+//                list_ars();
+                break;
+            case 4:
+                char search_choice;
+                cout << "Enter search type (A or B): ";
+                cin >> search_choice;
+                if (search_choice == 'A'){
+				}
+//                    search_car_by_id();
+                else if (search_choice == 'B')
+                {
+				}
+//                    search_car_by_name();
+                else
+                    cout << "Invalid search type." << endl;
+                break;
+            case 5:
+//                update_car_info();
+                break;
+            case 6:
+//                sort_cars_by_Id();
+                break;
+            case 7:
+//                sort_cars_by_name();
+                break;
+            case 8:
+                cout << "Exiting program..." << endl;
+                break;
+            default:
+                cout << "Invalid choice. Please enter a valid menu option." << endl;
+                break;
+        }
+
+        cout << endl;
+    } while (choice != 8);
+
 	
 	
 	return 0;
